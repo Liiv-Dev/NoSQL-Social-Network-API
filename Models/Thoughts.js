a0024999bb2e305b70ce9a09eec2c23935fc1dca
Willie Jones <williejones432@yahoo.com>
2024-01-30 11:46:21 -0600

    updated data structure in Thoughts Module

---
+++ Models/Thoughts.js
@@ -0,0 +1,34 @@
+const { Schema, Types } = require('mongoose');
+
+// Schema to create a new thought
+const thoughtSchema = new Schema({
+
+    thoughtText: {
+        type: String,
+        required: true,
+        min: 1,
+        max: 280
+    },
+
+    createdAt: {
+        type: Date, // Default value is a timestamp
+        default: Date.now, // Use a getter method to format the timestamp on query
+        // Use a getter method to format the timestamp on query
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+
+    username: { // The user that created this thought
+        type: String,
+        required: true
+    },
+
+     // array of nested documents created with the reactionSchema
+    reactions: [reactionSchema],
+
+})
+
+// A virtual that retrieves the length of the thought's reactions array field on query
+
+thoughtSchema.virtual('reactionSchema').get(function(){
+    return this.reactions.length;
+})
